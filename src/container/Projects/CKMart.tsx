46079aa8e08735a4a33f78fcfd8e73aa74030931
charles.labit <charleslabit@gmail.com>
2025-04-03 19:06:13 +0800

    update ck mart description

--- src/container/Projects/CKMart.tsx
+++ src/container/Projects/CKMart.tsx
@@ -18,11 +18,11 @@ export const CKMart = () => {
   return (
     <ProjectCard
       title="CK Mart - E-commerce Demo"
-      description="A full-stack e-commerce demo powered by Next.js, Supabase, and PostgreSQL. Currently, product and category management are implemented, with further enhancements in progress. Stripe.js is integrated for payments."
+      description="A full-stack e-commerce demo powered by Next.js, Supabase, and PostgreSQL. Currently, product, category and inventory management are implemented, with further enhancements in progress. Stripe.js is integrated for payments."
       image={"/default/e-commerce-products.png"}
       techStack={stack}
       liveDemo="https://ecommerce-demo-orpin.vercel.app/"
-      overlayDescription="A full-stack e-commerce demo powered by Next.js, Supabase, and PostgreSQL. Currently, product and category management are implemented, with further enhancements in progress. Stripe.js is integrated for payments. More features coming soon!"
+      overlayDescription="A full-stack e-commerce demo powered by Next.js, Supabase, and PostgreSQL. Currently, product, category and inventory management are implemented, with further enhancements in progress. Stripe.js is integrated for payments. More features coming soon!"
     />
   );
 };
