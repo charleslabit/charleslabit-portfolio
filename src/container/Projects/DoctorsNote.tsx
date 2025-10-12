26cf01bd3336f79edcb60cbf4808640880a1db9e
charles.labit <charleslabit@gmail.com>
2025-10-12 15:57:09 +0800

    replace technical exam into assessment

--- src/container/Projects/DoctorsNote.tsx
+++ src/container/Projects/DoctorsNote.tsx
@@ -5,14 +5,14 @@ const stack = ["Next.js", "TypeScript", "Javscript", "Mantine UI", "Vercel"];
 export const DoctorsNote = () => {
   return (
     <ProjectCard
-      title="My Doctor's Note Clone - Technical Exam"
-      description="A static clone of My Doctor's Note, created for a technical assessment."
+      title="My Doctor's Note Clone - Assessment Exam"
+      description="A static clone of My Doctor's Note, created for a assessment."
       image="/default/my-doctors-note.png"
       techStack={stack}
       liveDemo="https://my-doctors-note-six.vercel.app/"
       github="https://github.com/charleslabit/my-doctors-note"
       referenceSite="https://mydoctorsnote.co"
-      overlayDescription="A static clone of My Doctor's Note, created for a technical assessment."
+      overlayDescription="A static clone of My Doctor's Note, created for a assessment."
     />
   );
 };
